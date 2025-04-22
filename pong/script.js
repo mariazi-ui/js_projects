84f3666729649e25773c106298258af7d19a7aa9
marisgit-dev <183960467+mariazi-ui@users.noreply.github.com>
2025-04-22 14:01:35 -0400

    Created boeard for pong game

---
+++ pong/script.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("#canvas");
+let ctx = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, width, height);
