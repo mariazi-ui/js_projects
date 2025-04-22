6ffeb1ad3fea6d9734c1baa8a12ec29f9d99acf2
marisgit-dev <183960467+mariazi-ui@users.noreply.github.com>
2025-04-22 14:04:39 -0400

    Debugged loading issues with html and js

--- pong/script.js
+++ pong/script.js
@@ -1,7 +1,9 @@
-let canvas = document.getElementById("#canvas");
-let ctx = canvas.getContext("2d");
-let width = canvas.width;
-let height = canvas.height;
+window.onload = function () {
+    let canvas = document.getElementById("#canvas");
+    let ctx = canvas.getContext("2d");
+    let width = canvas.width;
+    let height = canvas.height;
 
-ctx.fillStyle = "#000";
-ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, width, height);
+}
